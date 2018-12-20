ef7bfbbf939fb997fdd4bed1a792108b302d06c9
odiwml147 <odiwml147@163.com>
2018-12-21 00:36:12 +0800

    店铺主页和购物车完成一部分

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,7 +9,9 @@ import {
   RESET_USER_INFO,
   RECEIVE_GOODS,
   RECEIVE_RATINGS,
-  RECEIVE_INFO
+  RECEIVE_INFO,
+  INCREMENT_FOOD_COUNT,
+  DECREMENT_FOOD_COUNT
 } from './mutation-types'
 
 import {
@@ -102,11 +104,22 @@ export default {
   },
 
   // 异步获取商家商品列表
-  async getShopGoods({commit}) {
+  async getShopGoods({commit}, callback) {
     const result = await reqShopGoods()
     if (result.code === 0) {
       const goods = result.data
       commit(RECEIVE_GOODS, {goods})
+      // 数据更新后 通知一下组件
+      callback && callback()
     }
   },
+
+  // 同步更新food中的count值
+  updateFoodCount({commit}, {isAdd, food}) {
+    if (isAdd) {
+      commit(INCREMENT_FOOD_COUNT, {food})
+    }else {
+      commit(DECREMENT_FOOD_COUNT, {food})
+    }
+  }
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,8 @@
 /*
 直接更新state的多个方法的对象
  */
+import Vue from 'vue'
+
 import {
   RECEIVE_ADDRESS,
   RECEIVE_CATEGORYS,
@@ -9,7 +11,9 @@ import {
   RESET_USER_INFO,
   RECEIVE_INFO,
   RECEIVE_RATINGS,
-  RECEIVE_GOODS
+  RECEIVE_GOODS,
+  INCREMENT_FOOD_COUNT,
+  DECREMENT_FOOD_COUNT
 } from './mutation-types'
 
 export default {
@@ -44,4 +48,44 @@ export default {
   [RECEIVE_GOODS](state, {goods}) {
     state.goods = goods
   },
+
+  // [INCREMENT_FOOD_COUNT](state, {food}) {
+  //   if (!food.count) {  // 第一次增加
+  //     // food.count = 1    // 新增属性（没有数据绑定效果）
+  //     /*
+  //     对象
+  //     属性名
+  //     属性值
+  //      */
+  //     Vue.set(food, 'count', 1)   // 让新增的属性也有数据绑定
+  //   } else {
+  //     food.count++
+  //   }
+  // },
+
+  [INCREMENT_FOOD_COUNT](state, {food}) {
+    if(!food.count) { // 第一次增加
+      // food.count = 1  // 新增属性(没有数据绑定)
+      /*
+      对象
+      属性名
+      属性值
+       */
+      Vue.set(food, 'count', 1) // 让新增的属性也有数据绑定
+      // 将food添加到cartFoods
+      state.cartFoods.push(food)
+    } else {
+      food.count++
+    }
+  },
+
+  [DECREMENT_FOOD_COUNT](state, {food}) {
+    if (food.count) {   // 只有有值才去减
+      food.count--
+      if (food.count === 0) {
+        // 将food从cartFoods中移除
+        state.cartFoods.splice(state.cartFoods.indexOf(food), 1)
+      }
+    }
+  }
 }
